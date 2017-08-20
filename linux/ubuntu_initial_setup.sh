d9768e3c21b3bba2aec0f814027b5a11ef352b03
Jamie Thompson <jamielthompson29.jt@gmail.com>
2017-08-20 15:08:24 -0500

    Added commands to install chrome
    Found some commands to install chrome via command line here https://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
    
    Installing chrome is always something I do, so just one more thing I wanted to try to automate. :)

--- linux/ubuntu_initial_setup.sh
+++ linux/ubuntu_initial_setup.sh
@@ -6,6 +6,11 @@
 apt update
 apt full-upgrade -y
 
+# Install Chrome
+apt-get install libxss1 libappindicator1 libindicator7 -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+
 # Install Node & npm
 apt-get install nodejs -y
 apt-get install npm -y
