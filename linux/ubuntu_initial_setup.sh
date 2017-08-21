07d18a0fc88dc82446182e7d21bf9b7418f39fdb
Jamie Thompson <jamielthompson29.jt@gmail.com>
2017-08-20 17:29:10 -0700

    Install Sublime
    Found commands via sublime (http://www.sublimetext.com/docs/3/linux_repositories.html) to install sublime.

--- linux/ubuntu_initial_setup.sh
+++ linux/ubuntu_initial_setup.sh
@@ -14,3 +14,8 @@ dpkg -i google-chrome*.deb
 # Install Node & npm
 apt-get install nodejs -y
 apt-get install npm -y
+
+# Install Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install sublime-text
